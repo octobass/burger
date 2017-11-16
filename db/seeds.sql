3b70bf6ac260e89ff219a3e8ff59506018f48d09
Kevin Lesane <kjl738@utexas.edu>
2017-11-16 09:44:58 -0600

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('McDonald\'s');
+INSERT INTO burgers (name) VALUES ('Burger King');
+INSERT INTO burgers (name, devoured) VALUES ('White Castle', true);
+INSERT INTO burgers (name, devoured) VALUES ('Wendy\'s', true);
+INSERT INTO burgers (name, devoured) VALUES ('P. Terry\'s', true);
+INSERT INTO burgers (name) VALUES ('Five Guys');
